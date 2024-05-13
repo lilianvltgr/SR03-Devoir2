60ded596dd27c5cbd81e7af7cec4b248a56ad9f5
Lilian <lilian.voltigeur@etu.utc.fr>
2024-05-13 11:47:34 +0200

    Add admin Authentification

--- src/main/java/fr/utc/sr03/chat_admin/controller_web/AdminController.java
+++ src/main/java/fr/utc/sr03/chat_admin/controller_web/AdminController.java
@@ -2,11 +2,12 @@ package fr.utc.sr03.chat_admin.controller_web;
 
 import fr.utc.sr03.chat_admin.database.UserRepository;
 import fr.utc.sr03.chat_admin.model.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 
@@ -31,6 +32,31 @@ public class AdminController {
         model.addAttribute("users", users);
         return "userList";
     }
+
+@GetMapping("/authentification")
+public String authentification(){
+        return "AuthentificationAdmin";
+}
+    @PostMapping("/isAdmin")
+    public String isAdmin(Model model, WebRequest request,
+                          @RequestParam("password") String password,
+                          @RequestParam("email") String email){
+        List<User> users = userRepository.findAll();
+        List<User> filteredUsers;
+        for (User entry: users)
+        {
+            // filtre les valeurs qui commencent par `B`
+            if (entry.getMail().equals(email) && (entry.getPassword().equals(password))){
+                model.addAttribute("currentAdmin", email);
+                request.setAttribute("email", email, WebRequest.SCOPE_SESSION );
+                return "currentAdmin";
+            }
+        }
+        model.addAttribute("currentAdmin", "");
+        return "currentAdmin";
+        //Connexion Invalide
+    }
+
     @GetMapping("/getAdmins")
     public List<User> getAdmins() {
         List<User> admins = userRepository.findAdminOnly();
@@ -40,7 +66,7 @@ public class AdminController {
     public String getAdmins(Model model) {
         List<User> admins = userRepository.findAdminOnly();
         model.addAttribute("admins", admins);
-        return "templateTest";  // Assurez-vous que ceci correspond au nom du fichier dans /src/main/resources/templates
+        return "adminList";  // Assurez-vous que ceci correspond au nom du fichier dans /src/main/resources/templates
     }
 
 }
