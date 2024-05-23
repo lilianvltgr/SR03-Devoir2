bca81a7f17a4bf7942e81c942d30456b28efed90
Lilian <lilian.voltigeur@etu.utc.fr>
2024-05-23 13:21:59 +0200

    update search bar

--- src/main/resources/static/JS/searchFunctions.js
+++ src/main/resources/static/JS/searchFunctions.js
@@ -1,19 +1,25 @@
-    function search() {
+function search() {
     // Declare variables
+    console.log("new input")
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
     ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
-
+    console.log(li.length)
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-    span = li[i].getElementsByTagName("span")[3];
-    console.log(span)
-    txtValue = span.textContent || span.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-    li[i].style.display = "";
-} else {
-    li[i].style.display = "none";
+        span = li[i].getElementsByTagName("span")[3];
+        console.log(span)
+        txtValue = span.textContent || span.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            console.log("show" + txtValue)
+
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+            console.log("hide" + txtValue)
+
+        }
+    }
 }
-}}
