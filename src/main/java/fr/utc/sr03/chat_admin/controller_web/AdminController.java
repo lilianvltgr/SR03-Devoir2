83b5d68f8c51a6a9c052e8d4fe5fbb142989f50f
Lilian <lilian.voltigeur@etu.utc.fr>
2024-06-16 19:12:47 +0200

    clean project

--- src/main/java/fr/utc/sr03/chat_admin/controller_web/AdminController.java
+++ src/main/java/fr/utc/sr03/chat_admin/controller_web/AdminController.java
@@ -17,6 +17,9 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Controller managing admin interface in the application
+ */
 @Controller
 @RequestMapping("/AdminController")
 public class AdminController {
@@ -25,8 +28,14 @@ public class AdminController {
     private AdminController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    /**
+     * Function used to get every user on the database and display it.
+     * @return a template
+     */
     @GetMapping("/users")
     public String getUserList(Model model, WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
@@ -36,29 +45,41 @@ public class AdminController {
         return "userList";
     }
 
+    /**
+     * Function used to get specific user information and display it.
+     * @return a template
+     */
     @GetMapping("/userInfos/{userId}")
     public String getUserInfos(Model model, @PathVariable Long userId, WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
         }
+        // Looking for the user
         Optional<User> userOptional = userRepository.findByUserId(userId);
         if (userOptional.isPresent()) {
             User userInfos = userOptional.get();
+            // Adding user infos to the model
             model.addAttribute("userInfos", userInfos);
         }
         return "userInfos";
     }
-
+    /**
+     * Function used to get every inactive user on the database and display it with pagination.
+     * @return a template
+     */
     @GetMapping("/inactiveUsers")
     public String getInactiveUsers(Model model, WebRequest request,
                                    @RequestParam("page") Optional<Integer> page,
                                    @RequestParam("lastname") Optional<String> lastname,
                                    @RequestParam("sort") Optional<Integer> sort) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
         }
+        // Gestion of the sort
         Sort sortCriteria = Sort.by(Sort.Direction.ASC, "lastname", "firstname");
         switch (sort.orElse(0)) {
             case 0:
@@ -68,7 +89,6 @@ public class AdminController {
                 sortCriteria = Sort.by(Sort.Direction.ASC, "lastname", "lastname");
                 break;
         }
-        // Recup des users
         Page<User> users = userRepository.findByActiveFalseAndLastnameContainingIgnoreCase(lastname.orElse(""), PageRequest.of(page.orElse(0), 2, sortCriteria));
         model.addAttribute("users", users.getContent());
         model.addAttribute("current_page", page.orElse(0));
@@ -78,61 +98,87 @@ public class AdminController {
         model.addAttribute("userPage", users);
         return "userList";
     }
-
+    /**
+     * Function used to get the new user form
+     * @return a template
+     */
     @GetMapping("/getUserForm")
     public String getUserForm(Model model, WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
         }
-        model.addAttribute("user", new User()); // Ajoutez cette ligne pour lier le formulaire
+        // Add the user to the model to link it to the template
+        model.addAttribute("user", new User());
         return "newUserForm";
     }
 
-
+    /**
+     * Function used go back to the Home Page.
+     * @return the home page template
+     */
     @GetMapping("")
     public String goHome(WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
         }
         return "homePage";
     }
-
+    /**
+     * Function used to display the authentification template
+     * @return a template
+     */
     @GetMapping("/authentification")
     public String authentification() {
         return "authentificationAdmin";
     }
-
-    @GetMapping("/deconnexion")
-    public String deconnexion(WebRequest request) {
+    /**
+     * Function that "disconnects" the user by removing sessions attributes and forcing him to authentificate again
+     * @return the home Page
+     */
+    @GetMapping("/disconnection")
+    public String disconnection(WebRequest request) {
         request.removeAttribute("email", WebRequest.SCOPE_SESSION);
         request.removeAttribute("isAdmin", WebRequest.SCOPE_SESSION);
         request.removeAttribute("connected", WebRequest.SCOPE_SESSION);
-
         return "redirect:/AdminController";
     }
+//    /**
+//     * Function that redirects
+//     * @return a template
+//     */
+//    @GetMapping("/redirectAuth")
+//    public String redirectAuth(WebRequest request) {
+//        Object isAdmin = request.getAttribute("isAdmin", WebRequest.SCOPE_SESSION);
+//        if (isAdmin != null && isAdmin.toString().equals("true")) {
+//            return "redirect:/AdminController/adminHomePage";
+//        }
+//        return "redirect:/AdminController/authentification";
+//    }
 
-    @GetMapping("/redirectAuth")
-    public String redirectAuth(WebRequest request) {
-        Object isAdmin = request.getAttribute("isAdmin", WebRequest.SCOPE_SESSION);
-        if (isAdmin != null && isAdmin.toString().equals("true")) {
-            return "redirect:/AdminController/adminHomePage";
-        }
-        return "redirect:/AdminController/authentification";
-    }
-
+    /**
+     * Function used to access the admin home page
+     * @return a template
+     */
     @GetMapping("/adminHomePage")
     public String adminHomePage(WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
         }
         return "adminHomePage";
     }
-
+    /**
+     * Function to add an user to the database with validation
+     * @return a template
+     */
     @PostMapping("/addUser")
     public String postTruc(@ModelAttribute @Valid User user, BindingResult result, Model model, WebRequest request) {
+        // Authentification verification
         Object connected = request.getAttribute("connected", WebRequest.SCOPE_SESSION);
         if (connected == null || !connected.toString().equals("true")) {
             return "authentificationAdmin";
